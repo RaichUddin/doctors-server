14df2276b90467a99734ef260acf334b620ef0ec
raich uddin <rabbitrade10@gmail.com>
2021-12-15 01:17:27 +0600

    added service

---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require('express')
+const admin = require("firebase-admin");
+const app = express()
+require('dotenv').config()
+const cors = require('cors');
+var MongoClient = require('mongodb').MongoClient;
+const port = process.env.PORT || 5000;
+
+
+
+
+const serviceAccount = require('./doctor-portal-token.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+app.use(cors());
+app.use(express.json());
+
+var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.00ohf.mongodb.net:27017,cluster0-shard-00-01.00ohf.mongodb.net:27017,cluster0-shard-00-02.00ohf.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-ko9svf-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
+MongoClient.connect(uri, function (err, client) {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+    }
+    next();
+}
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('doctors_client');
+        const appointmentCollection = database.collection('appointments');
+        const usersCollection = database.collection('users');
+
+        app.get('/appointments', async (req, res) => {
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString()
+
+            const query = { email: email, date: date }
+            console.log(query);
+            const cursor = appointmentCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+        })
+
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentCollection.insertOne(appointment);
+
+            res.json(result);
+        });
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        });
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+
+            res.json(result);
+
+        });
+
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+            console.log('decodedEmail', req.decodedEmail);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello doctors portal')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening ${port}`)
+})
